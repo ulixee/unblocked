3fb2503e034e5076069cfd383c3554d3cff01a60
wireguard-dev <133251816+bruhabuser@users.noreply.github.com>
2023-05-11 19:13:12 +0200

    override performance.getEntries

--- plugins/default-browser-emulator/injected-scripts/performance.ts
+++ plugins/default-browser-emulator/injected-scripts/performance.ts
@@ -16,3 +16,18 @@ proxyFunction(
     return entries;
   },
 );
+
+proxyFunction(performance, 'getEntries', (target, thisArg, argArray): PerformanceEntryList => {
+  let entries: PerformanceEntryList = ReflectCached.apply(target, thisArg, argArray);
+
+  entries.forEach(entry => {
+    if (entry.entryType === 'navigation') {
+      //@ts-expect-error
+      proxyGetter(entry, 'activationStart', () => 0);
+      //@ts-expect-error
+      proxyGetter(entry, 'renderBlockingStatus', () => 'non-blocking');
+    }
+  });
+
+  return entries;
+});
