d6798b706a2dce189136a22bf18bc6f1d973f94b
brecht stamper <stamper.brecht@gmail.com>
2024-05-03 13:39:17 +0200

    only hide objects

--- plugins/default-browser-emulator/injected-scripts/console.ts
+++ plugins/default-browser-emulator/injected-scripts/console.ts
@@ -1,16 +1,19 @@
-const hideAllLogs = true;
-
 // Logging error on any of these levels triggers a getter, which could be proxied.
 // The same could technically be done for any object so to prevent detection here
 // we have to disable all console logging functionality, which is pretty annoying
 // and hopefully we find a better solution for this.
-
 const logLevels = ['trace', 'debug', 'info', 'warn', 'error', 'log'] as const;
+const reflectCached = ReflectCached;
 
-if (hideAllLogs) {
-  for (const logLevel of logLevels) {
-    proxyFunction(console, logLevel, () => {
-      return undefined;
+for (const logLevel of logLevels) {
+  proxyFunction(console, logLevel, (target, thisArg, args) => {
+    const safeArgs = args.map(arg => {
+      if (typeof arg === 'object') {
+        return 'hiding arg';
+      }
+      return arg;
     });
-  }
+
+    return reflectCached.apply(target, thisArg, safeArgs);
+  });
 }
