ae77191b73c329e361b6e483249f0e76b2d45b3a
brecht stamper <stamper.brecht@gmail.com>
2024-05-06 10:29:22 +0200

    fix: even more console leaks

--- plugins/default-browser-emulator/injected-scripts/console.ts
+++ plugins/default-browser-emulator/injected-scripts/console.ts
@@ -1,12 +1,25 @@
-// Logging error on any of these levels triggers a getter, which could be proxied.
+// Logging error on any of these keys triggers a getter, which could be proxied.
 // The same could technically be done for any object so to prevent detection here
 // we use json stringify, so we ignore all of this dangerous logic while still
 // logging as much as possible of the original object.
-const logLevels = ['trace', 'debug', 'info', 'warn', 'error', 'log'] as const;
+const keysToModifyArgs = [
+  'trace',
+  'debug',
+  'info',
+  'warn',
+  'error',
+  'log',
+  'dir',
+  'dirxml',
+  'table',
+  'group',
+  'groupCollapsed',
+] as const;
+const KeysToDropArgs = ['groupEnd', 'clear'] as const;
 const reflectCached = ReflectCached;
 
-for (const logLevel of logLevels) {
-  proxyFunction(console, logLevel, (target, thisArg, args) => {
+for (const key of keysToModifyArgs) {
+  proxyFunction(console, key, (target, thisArg, args) => {
     const safeArgs = args.map(arg => {
       if (typeof arg === 'object') {
         return JSON.stringify(arg, null, 2);
@@ -15,5 +28,11 @@ for (const logLevel of logLevels) {
     });
 
     return reflectCached.apply(target, thisArg, safeArgs);
+  });
+}
+
+for (const key of KeysToDropArgs) {
+  proxyFunction(console, key, (target, thisArg, _args) => {
+    return reflectCached.apply(target, thisArg, []);
   });
 }
